2f5a4d50e0796538f48ab4b3eed16341398f3969
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-03-16 23:54:08 -0700

    Add Subtract Division and multiplication completed (Fully working) Seperate buttons have to be implemented

--- app/src/main/java/com/mystikoAmila/calculator/MainActivity.java
+++ app/src/main/java/com/mystikoAmila/calculator/MainActivity.java
@@ -15,11 +15,19 @@ public class MainActivity extends AppCompatActivity {
  Button mulButton;
  Button divButton;
  Button getButton;
-double x=0;
+double x=0.0;
 int buttonCliked=0;
 int FirstValEntered=0;
-double val;
-double y=1;
+double val=0.0;
+double y=1.0;
+
+int divVal=0;
+double Firstval=0.0;
+double divAns=0.0;
+
+    int subVal=0;
+    double Firstvalsub=0.0;
+    double subAns=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,27 +54,34 @@ double y=1;
                         double Num=Double.parseDouble(idEditText.getText().toString());
                         x=x+Num;
                         idEditText.setText(String.valueOf(x));
-                        x = 0.0;
+                        //x = 0.0;
                     }}
-                    else if(buttonCliked==2){
-                        if(TextUtils.isEmpty((idEditText.getText().toString()))){
-                         double Num=Double.parseDouble(idEditText.getText().toString());
-                         val=val-Num;
-                         idEditText.setText(String.valueOf(val));
-                         //val=0.0;
-                        }
-                    }
+
                 if(buttonCliked==3) {
                     if (!TextUtils.isEmpty(idEditText.getText().toString())) {
                         double Num=Double.parseDouble(idEditText.getText().toString());
                         y=y*Num;
                         idEditText.setText(String.valueOf(y));
-                        y= 0.0;
+                        //y= 0.0;
                     }}
-                    else{
+                if(buttonCliked==4) {
+                    if (!TextUtils.isEmpty(idEditText.getText().toString())) {
+                        double Num=Double.parseDouble(idEditText.getText().toString());
+                        Firstval=Firstval/Num;
+                        idEditText.setText(String.valueOf(Firstval));
+                        //x = 0.0;
+                    }}
+                if(buttonCliked==2) {
+                    if (!TextUtils.isEmpty(idEditText.getText().toString())) {
+                        double Num=Double.parseDouble(idEditText.getText().toString());
+                        Firstvalsub=Firstvalsub-Num;
+                        idEditText.setText(String.valueOf(Firstvalsub));
+                        //x = 0.0;
+                    }}
+                   /* else{
                         idEditText.setText(String.valueOf(x));
                         x=0.0;
-                    }
+                    }*/
 
                          }
      });
@@ -133,9 +148,19 @@ if(type.equals("+")){
         if(type.equals("/")){
             buttonCliked=4;
             double Number= Double.parseDouble(number);
-            x=x+Number;
-            Toast.makeText(this, String.valueOf(x), Toast.LENGTH_SHORT).show();
-            idEditText.setText("");
+
+            if(divVal==0){
+                Firstval=Number;
+                divVal++;
+                Toast.makeText(this, String.valueOf(Firstval), Toast.LENGTH_SHORT).show();
+                idEditText.setText("");
+            }
+            else{
+                divAns=Firstval/Number;
+                Firstval=divAns;
+                Toast.makeText(this, String.valueOf(divAns), Toast.LENGTH_SHORT).show();
+                idEditText.setText("");
+            }
         }
 
 
@@ -150,25 +175,23 @@ if(type.equals("+")){
             idEditText.setText("");
         }
 
-if(type.equals("-") ){
-    buttonCliked=2;
+        if(type.equals("-")){
+            buttonCliked=2;
+            double Number= Double.parseDouble(number);
 
-    if(FirstValEntered==0) {
-        double Number=Double.parseDouble(number);
-        val = Number;
-        FirstValEntered = 1;
-        Toast.makeText(this, String.valueOf(val), Toast.LENGTH_SHORT).show();
-        idEditText.setText("");
-    }
-    if(FirstValEntered==1){
-        double Number2=Double.parseDouble(number);
-        val=val-Number2;
-        Toast.makeText(this, String.valueOf(val), Toast.LENGTH_SHORT).show();
-        idEditText.setText("");
-
-    }
-
-}
+            if(subVal==0){
+                Firstvalsub=Number;
+                subVal++;
+                Toast.makeText(this, String.valueOf(Firstvalsub), Toast.LENGTH_SHORT).show();
+                idEditText.setText("");
+            }
+            else{
+                subAns=Firstvalsub-Number;
+                Firstvalsub=subAns;
+                Toast.makeText(this, String.valueOf(subAns), Toast.LENGTH_SHORT).show();
+                idEditText.setText("");
+            }
+        }
 
     }
 }
